5d162bb712447fcee5bee877a6cb2c8402db0908
Frank Yu <frank_yzg@outlook.com>
2021-12-27 17:49:58 +0000

    Rename procedure to api

--- src/Test/HAPI/Procedure.hs
+++ src/Test/HAPI/Procedure.hs
@@ -30,116 +30,120 @@ import Control.Carrier.Writer.Strict (WriterC, tell)
 import Text.Read (readMaybe)
 
 
-class HaskellFnDef (def :: (* -> *) -> * -> *) where
-  evalHaskell :: def m a -> a
+type ApiDefinition = (* -> *) -> * -> *
+type PropertyType = (* -> *) -> * -> *
 
-class HaskellIOCall (def :: (* -> *) -> * -> *) where
-  showArgs :: def m a -> String
-  readOut :: def m a -> String -> Maybe a
+class HasHaskellDef (api :: ApiDefinition) where
+  evalHaskell :: api m a -> a
 
-data ProcedureA (m :: * -> *) a where
-  AddA :: Int -> Int -> ProcedureA m Int
-  SubA :: Int -> Int -> ProcedureA m Int
-  MulA :: Int -> Int -> ProcedureA m Int
-  NegA :: Int -> ProcedureA m Int
-  StrA :: Int -> ProcedureA m String
+class HaskellIOCall (api :: ApiDefinition) where
+  showArgs :: api m a -> String
+  readOut :: api m a -> String -> Maybe a
 
-deriving instance Show (ProcedureA m a)
+data ArithApiA (m :: * -> *) a where
+  AddA :: Int -> Int -> ArithApiA m Int
+  SubA :: Int -> Int -> ArithApiA m Int
+  MulA :: Int -> Int -> ArithApiA m Int
+  NegA :: Int -> ArithApiA m Int
 
-instance HaskellFnDef ProcedureA where
+data ShowApiA (m :: * -> *) a where
+  StrA :: Int -> ShowApiA m String
+
+deriving instance Show (ArithApiA m a)
+deriving instance Show (ShowApiA m a)
+
+instance HasHaskellDef ShowApiA where
+  evalHaskell (StrA a) = show a
+
+instance HaskellIOCall ShowApiA where
+  readOut (StrA _) = readMaybe
+  showArgs = show
+
+instance HasHaskellDef ArithApiA where
   evalHaskell (AddA a b) = a + b
   evalHaskell (SubA a b) = a - b
   evalHaskell (MulA a b) = a * b
   evalHaskell (NegA a)   = -a
-  evalHaskell (StrA a)   = show a
 
-instance HaskellIOCall ProcedureA where
-  readOut (StrA _) = readMaybe
+instance HaskellIOCall ArithApiA where
   readOut (AddA _ _) = readMaybe
   readOut (SubA _ _) = readMaybe
   readOut (MulA _ _) = readMaybe
   readOut (NegA _) = readMaybe
   showArgs = show
 
-addA, subA, mulA :: Has ProcedureA sig m => Int -> Int -> m Int
+addA, subA, mulA :: Has ArithApiA sig m => Int -> Int -> m Int
 addA a b = send $ AddA a b
 subA a b = send $ SubA a b
 mulA a b = send $ MulA a b
 
-negA :: Has ProcedureA sig m => Int -> m Int
+negA :: Has ArithApiA sig m => Int -> m Int
 negA a = send $ NegA a
 
-strA :: Has ProcedureA sig m => Int -> m String
+strA :: Has ShowApiA sig m => Int -> m String
 strA a = send $ StrA a
 
--- | Encode procedure's type into its underlying interpretation to ensure functional dependency for Algebra holds
-newtype ProcedureAC (proc :: (* -> *) -> * -> *) m a = ProcedureAC {runProcedureAC :: ReaderC () m a}
+-- | Encode api call's type into its underlying interpretation to ensure functional dependency for Algebra holds
+newtype ApiAC (api :: ApiDefinition) m a = ApiAC { runApiAC :: ReaderC () m a }
   deriving (Functor, Applicative, Monad, MonadIO, MonadFail)
 
-runProcedure :: ProcedureAC proc m a -> m a
-runProcedure = runReader () . runProcedureAC
+runApi :: ApiAC proc m a -> m a
+runApi = runReader () . runApiAC
 
-newtype ProcedureRpcAC (proc :: (* -> *) -> * -> *) m a = ProcedureRpcAC { runProcedureRpcAC :: ReaderC () m a }
+newtype ApiRpcAC (api :: ApiDefinition) m a = ApiRpcAC { runApiRpcAC :: ReaderC () m a }
   deriving (Functor, Applicative, Monad, MonadIO, MonadFail)
 
-runProcedureRpc :: ProcedureRpcAC proc m a -> m a
-runProcedureRpc = runReader () . runProcedureRpcAC
+runApiRpc :: ApiRpcAC proc m a -> m a
+runApiRpc = runReader () . runApiRpcAC
 
--- | If the procedure can map to relevant haskell functions, then it can be interpreted
-instance (Algebra sig m, HaskellFnDef proc) => Algebra (proc :+: sig) (ProcedureAC proc m) where
-  alg hdl sig ctx = ProcedureAC $ case sig of
-    L procedure -> return (ctx $> evalHaskell procedure)
-    R other -> alg (runProcedureAC . hdl) (R other) ctx
+-- | If the api call can map to relevant haskell functions, then it can be interpreted
+instance (Algebra sig m, HasHaskellDef api) => Algebra (api :+: sig) (ApiAC api m) where
+  alg hdl sig ctx = ApiAC $ case sig of
+    L call -> return (ctx $> evalHaskell call)
+    R other -> alg (runApiAC . hdl) (R other) ctx
 
-instance (Algebra sig m, MonadIO m, MonadFail m, HaskellIOCall proc) => Algebra (proc :+: sig) (ProcedureRpcAC proc m) where
-  alg hdl sig ctx = ProcedureRpcAC $ case sig of
-    L procedure -> do
-      liftIO $ putStrLn $ showArgs procedure
-      out <- liftIO (readOut procedure <$> getLine)
+instance (Algebra sig m, MonadIO m, MonadFail m, HaskellIOCall proc) => Algebra (proc :+: sig) (ApiRpcAC proc m) where
+  alg hdl sig ctx = ApiRpcAC $ case sig of
+    L call -> do
+      liftIO $ putStrLn $ showArgs call
+      out <- liftIO (readOut call <$> getLine)
       case out of
         Nothing -> fail "Parse error"
         Just o  -> return (ctx $> o)
-    R other -> alg (runProcedureRpcAC . hdl) (R other) ctx
+    R other -> alg (runApiRpcAC . hdl) (R other) ctx
 
+data PropertyTypeA (m :: * -> *) a where
+  ShouldBeA :: Eq a => a -> a -> PropertyTypeA m Bool
 
-show3Plus5Min2 :: Has ProcedureA sig m => m String
-show3Plus5Min2 = do
-  eight <- addA 3 5
-  subA eight 2 >>= strA
+shouldBe :: (Eq a, Has PropertyTypeA sig m) => a -> a -> m Bool
+shouldBe a b = send $ ShouldBeA a b
 
-x, y :: IO String
-x = runReader () $ runProcedureRpcAC @ProcedureA show3Plus5Min2
-y = runReader () $ runProcedureAC @ProcedureA show3Plus5Min2
-
-data PropertyA (m :: * -> *) a where
-  EqA :: Eq a => a -> a -> PropertyA m Bool
-
-(===) :: (Eq a, Has PropertyA sig m) => a -> a -> m Bool
-a === b = send $ EqA a b
-
-newtype PropertyAC (prop :: (* -> *) -> * -> *) m a = PropertyAC { runPropertyAC :: ReaderC () m a }
+newtype PropertyTypeAC (prop :: PropertyType) m a = PropertyTypeAC { runPropertyTypeAC :: ReaderC () m a }
   deriving (Functor, Applicative, Monad, MonadIO, MonadFail)
 
-runProperty :: PropertyAC prop m a -> m a
-runProperty = runReader () . runPropertyAC
+runPropertyType :: PropertyTypeAC prop m a -> m a
+runPropertyType = runReader () . runPropertyTypeAC
 
-class EvalProperty (prop :: (* -> *) -> * -> *) where
-  assertProp :: prop m a -> a
+class EvalPropertyType (prop :: PropertyType) where
+  evalPropertyType :: prop m a -> a
 
-instance EvalProperty PropertyA where
-  assertProp (EqA a b) = a == b
+instance EvalPropertyType PropertyTypeA where
+  evalPropertyType (ShouldBeA a b) = a == b
 
-instance (Algebra sig m, EvalProperty prop) => Algebra (prop :+: sig) (PropertyAC prop m) where
-  alg hdl sig ctx = PropertyAC $ case sig of
-     L prop -> return $ ctx $> assertProp prop
-     R other -> alg (runPropertyAC . hdl) (R other) ctx
+instance (Algebra sig m, EvalPropertyType prop) => Algebra (prop :+: sig) (PropertyTypeAC prop m) where
+  alg hdl sig ctx = PropertyTypeAC $ case sig of
+    L prop  -> return $ ctx $> evalPropertyType prop
+    R other -> alg (runPropertyTypeAC . hdl) (R other) ctx
 
-show3Plus5Is8 :: Has (ProcedureA :+: PropertyA) sig m => m Bool
+show3Plus5Is8 :: Has (ArithApiA :+: ShowApiA :+: PropertyTypeA) sig m => m Bool
 show3Plus5Is8 = do
   x <- addA 3 5
   x' <- strA x
   let y = "8"
-  x' === y
+  x' `shouldBe` y
 
 prop :: IO Bool
-prop = runProperty @PropertyA $ runProcedureRpc @ProcedureA show3Plus5Is8
+prop = runPropertyType @PropertyTypeA
+     $ runApiRpc @ArithApiA
+     $ runApi @ShowApiA
+     $ show3Plus5Is8
